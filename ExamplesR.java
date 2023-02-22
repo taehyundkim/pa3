483d30036d4306840748fdd55ffe35bedc827a59
Daniel Kim <taehyun.k0724@gmail.com>
2023-02-21 18:39:35 -0800

    revision commit 2

--- ExamplesR.java
+++ ExamplesR.java
@@ -13,5 +13,5 @@ class ExamplesR {
 
   R newObject = new R("testing", null);
 }
-// 1. I was not able to construct an R object because inside class R, it calls on another R object as its parameter. This would be impossible create as you need a initial R object to create a new object, which will be a infinite loop of not being able to create an object.
+// I was able to construct a R object using the null value for the example argument.
 //2. It is impossible to create a reply to a reply to a tweet because of the structure of the TextTweet and ReplyTweet class. ReplyTweet class uses TextTweet as its parameter, calling on the original tweet to base the reply tweet off of. So, when constructing a replyTweet object to reply to another replyTweet, it simply can't pass through a replyTweet as its parameter, because the code only allows a TextTweet to be its parameter. So replyTweet objects will only be able to reply to the original tweet and not to each other.
